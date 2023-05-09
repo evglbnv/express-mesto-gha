30bbc0160d0a6e52aff7d6d7519fda1be16494ee
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-05-09 10:01:34 +0300

    13 project 1.4

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 
+const {ERROR_CODE_NOT_FOUND,} = require('../utils/utils');
+
 const userRouter = require('./users');
 const cardRouter = require('./cards');
 
 router.use(userRouter);
 router.use(cardRouter);
+router.use('/*', (req, res)=> {
+  res.status(ERROR_CODE_NOT_FOUND).send({message: '404: Not Found'})
+})
 
 module.exports = router;
