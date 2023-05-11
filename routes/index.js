e3bf1b597f0a433c2238f1e275807fc560d00be7
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-05-11 19:27:47 +0300

    13 project 2.0. 1st iteration 1.1

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 
 router.use('/users', userRouter);
-router.use( '/cards' , cardRouter);
+router.use('/cards', cardRouter);
 router.use('/*', (req, res) => {
   res.status(ERROR_CODE_NOT_FOUND).send({ message: '404: Not Found' });
 });
