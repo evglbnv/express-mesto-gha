6a8225a282e5e90dec5c885c7215e7be32a56941
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-05-20 08:05:52 +0300

    14 project. Add Joi validation, centralized errorHandler, draft-delete, error fix 1

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,9 @@
 const authRouter = require('express').Router();
 
 const { createUser, login } = require('../controllers/users');
+const {validationSignin, validationSignUp} = require('../utils/validation')
 
-authRouter.post('/signup', createUser);
-authRouter.post('/signin', login);
+authRouter.post('/signup', validationSignUp, createUser);
+authRouter.post('/signin', validationSignin, login);
 
 module.exports = { authRouter };
