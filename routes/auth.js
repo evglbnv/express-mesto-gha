5c63185d440151818e3789e8f7bfe1a20a55a05d
Evgeniy Lobanov <evglbnv@yandex.ru>
2023-05-20 15:05:24 +0300

    14 project. routers updates

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const authRouter = require('express').Router();
-
-const { createUser, login } = require('../controllers/users');
-const { validationSignin, validationSignUp } = require('../utils/validation');
-
-authRouter.post('/signup', validationSignUp, createUser);
-authRouter.post('/signin', validationSignin, login);
-
-module.exports = { authRouter };
